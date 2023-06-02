3161abdff0ce4041632da99af1de6426ac00cc36
Ravishankarjanghel <ravijanghel@gmail.com>
2023-06-02 16:01:08 +0530

    1st.

---
+++ pipe2/src/test/java/simplePipe2/Pipe2TC2Test.java
@@ -0,0 +1,14 @@
+package simplePipe2;
+
+import org.testng.annotations.Test;
+
+public class Pipe2TC2Test {
+
+	@Test
+	public void pipe2Test() {
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		System.out.println(BROWSER);
+		System.out.println(URL);
+	}
+}
